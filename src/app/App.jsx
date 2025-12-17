1e34df90ee9e287ce9c7167bf67f13fe7789b0ac
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-12-17 12:26:29 +0530

    create the 2nd page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import HomePage from '../page/HomePage/HomePage.jsx'
+import ExamplePage from '../page/ExamplePage/ExamplePage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <HomePage />
+      {/* <HomePage /> */}
+      <ExamplePage/>
     </>
   )
 }
