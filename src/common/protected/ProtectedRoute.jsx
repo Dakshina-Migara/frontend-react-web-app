d90f312e863a83c27fbaa249f2a7899eb845cffe
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-12-21 09:17:14 +0530

    updates

--- src/common/protected/ProtectedRoute.jsx
+++ src/common/protected/ProtectedRoute.jsx
@@ -4,8 +4,11 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ stepRequired, children }) => {
   //useselector kiyanne data read karanna store eken kiyanne
+  //form kiyanne slice eke name eka
+  //step kiynne data store wenna ona me slice ekata
   const currentStep = useSelector(state => state.form.step);
 
+  //current page eke idala ilga page ekata yanna
   if (currentStep < stepRequired) {
     return <Navigate to="/firstpage" replace />;  
   }
